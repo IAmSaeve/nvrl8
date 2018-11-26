0bd407dbe5aa0cd88e090966cb7fbcaa581cebc5
Sebastian Petersen <sebastian@srmail.dk>
2018-11-26 12:13:32 +0100

    Leg done

--- TestApiRejseplanen/TestApiRejseplanen/Model/Leg.cs
+++ TestApiRejseplanen/TestApiRejseplanen/Model/Leg.cs
@@ -7,12 +7,43 @@ namespace TestApiRejseplanen.Model
 {
     public class Leg
     {
+        private string _type;
+
         public string Name { get; set; }
 
-        public enum Type { get; set; }
-
+        public string Type
         {
+            get => _type;
+            set
+            {
+                if (value.ToUpper() == "IC" ||
+                    value.ToUpper() == "LYN" ||
+                    value.ToUpper() == "REG" ||
+                    value.ToUpper() == "S" ||
+                    value.ToUpper() == "TOG" ||
+                    value.ToUpper() == "BUS" ||
+                    value.ToUpper() == "EXB" ||
+                    value.ToUpper() == "NB" ||
+                    value.ToUpper() == "TB" ||
+                    value.ToUpper() == "F" ||
+                    value.ToUpper() == "M" ||
+                    value.ToUpper() == "WALK" ||
+                    value.ToUpper() == "BIKE" ||
+                    value.ToUpper() == "CAR")
+                {
+                    _type = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid type");
+                }
+            }
+        }
 
+        public Leg(string name, string type)
+        {
+            Name = name;
+            Type = type;
         }
     }
 }
