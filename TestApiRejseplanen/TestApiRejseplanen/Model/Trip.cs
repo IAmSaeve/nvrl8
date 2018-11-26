6f212610dd6664c28b18905b8f23eaa206a64199
Sebastian Petersen <sebastian@srmail.dk>
2018-11-26 12:13:37 +0100

    Trip done

--- TestApiRejseplanen/TestApiRejseplanen/Model/Trip.cs
+++ TestApiRejseplanen/TestApiRejseplanen/Model/Trip.cs
@@ -7,5 +7,10 @@ namespace TestApiRejseplanen.Model
 {
     public class Trip
     {
+        public List<Trip> TripList { get; set; }
+        public List<Leg> Legs { get; set; }
+        public Origin Origin { get; set; }
+        public Destination Destination { get; set; }
+
     }
 }
