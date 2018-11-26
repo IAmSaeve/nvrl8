68be6b7eaa70eb53140f7d5637d325ab6d5cdfff
Sebastian Petersen <sebastian@srmail.dk>
2018-11-26 11:05:40 +0100

    Refactored mazegame

--- Python/MazeGame.py
+++ Python/MazeGame.py
@@ -3,86 +3,86 @@ from time import sleep
 
 sense = SenseHat()
 
-r = (255,0,0)
-b = (0,0,0)
-w = (255,255,255)
-g = (218,165,32)
-ng = (218,165,32)
+r = (255, 0, 0)
+b = (0, 0, 0)
+w = (255, 255, 255)
+g = (218, 165, 32)
+ng = (218, 165, 32)
 
 x = 1
 y = 1
 
+maze = [[r, r, r, r, r, r, r, r],
+        [r, b, b, b, b, b, b, g],
+        [r, r, r, b, r, b, b, r],
+        [r, b, r, b, r, r, r, r],
+        [r, b, b, b, b, b, b, r],
+        [r, b, r, r, r, r, b, r],
+        [r, b, b, r, b, b, b, r],
+        [r, r, r, r, r, g, r, r]]
 
-maze = [[r,r,r,r,r,r,r,r],
-        [r,b,b,b,b,b,b,g],
-        [r,r,r,b,r,b,b,r],
-        [r,b,r,b,r,r,r,r],
-        [r,b,b,b,b,b,b,r],
-        [r,b,r,r,r,r,b,r],
-        [r,b,b,r,b,b,b,r],
-        [r,r,r,r,r,g,r,r]]
-        
-maze2 =[[r,r,r,r,r,r,r,r],
-        [r,b,r,b,b,b,b,r],
-        [r,b,r,b,r,b,r,r],
-        [r,b,b,b,r,b,g,r],
-        [r,b,r,b,r,r,r,r],
-        [r,b,r,b,b,b,b,r],
-        [r,b,r,b,r,r,b,r],
-        [r,r,r,r,r,r,g,r]]
-		
-maze3 =[[r,r,r,r,b,r,b,r],
-        [r,r,b,b,b,b,b,r],
-        [r,g,b,r,r,b,r,r],
-        [r,r,r,r,r,b,r,r],
-        [r,b,b,b,b,b,b,r],
-        [r,r,b,r,r,r,r,r],
-        [r,r,b,b,b,b,b,b],
-        [r,r,r,r,r,r,g,r]]
+maze2 = [[r, r, r, r, r, r, r, r],
+         [r, b, r, b, b, b, b, r],
+         [r, b, r, b, r, b, r, r],
+         [r, b, b, b, r, b, g, r],
+         [r, b, r, b, r, r, r, r],
+         [r, b, r, b, b, b, b, r],
+         [r, b, r, b, r, r, b, r],
+         [r, r, r, r, r, r, g, r]]
 
+maze3 = [[r, r, r, r, b, r, b, r],
+         [r, r, b, b, b, b, b, r],
+         [r, g, b, r, r, b, r, r],
+         [r, r, r, r, r, b, r, r],
+         [r, b, b, b, b, b, b, r],
+         [r, r, b, r, r, r, r, r],
+         [r, r, b, b, b, b, b, b],
+         [r, r, r, r, r, r, g, r]]
 
-game_over = False        
+game_over = False
 
 
-def check_wall(x,y,new_x,new_y):
-  if maze[new_y][new_x] != r:
-    return new_x, new_y
-  elif maze[new_y][x] != r:
-    return x, new_y
-  elif maze[y][new_x] != r:
-    return new_x, y
-  else:
-    return x,y
+def check_wall(x, y, new_x, new_y):
+    if maze[new_y][new_x] != r:
+        return new_x, new_y
+    elif maze[new_y][x] != r:
+        return x, new_y
+    elif maze[y][new_x] != r:
+        return new_x, y
+    else:
+        return x, y
+
 
 def move_marble(pitch, roll, x, y):
-  new_x = x
-  new_y = y
-  if 1 < pitch < 179 and x != 0:
-    new_x -= 1
-  elif 181 < pitch < 359 and x != 7:
-    new_x += 1
-  elif 1 < roll < 179 and y != 7:
-    new_y += 1
-  elif 181 < roll < 359 and y != 0:
-    new_y -= 1
-  new_x, new_y = check_wall(x,y,new_x,new_y)
-  return new_x, new_y
+    new_x = x
+    new_y = y
+    if 1 < pitch < 179 and x != 0:
+        new_x -= 1
+    elif 181 < pitch < 359 and x != 7:
+        new_x += 1
+    elif 1 < roll < 179 and y != 7:
+        new_y += 1
+    elif 181 < roll < 359 and y != 0:
+        new_y -= 1
+    new_x, new_y = check_wall(x, y, new_x, new_y)
+    return new_x, new_y
 
-def Game_Start():
-	while game_over == False:
-		pitch = sense.get_orientation()['pitch']
-		roll = sense.get_orientation()['roll']
-		x,y = move_marble(pitch, roll, x,y)
-		if maze[y][x] == g:
-			sense.show_message("Level 2")
-			maze = maze2
-		if maze[y][x] == g:
-			sense.show_message("Level 3")
-			maze2 = maze3
-		if maze[y][x] == g:
-			sense.show_message("Win!!")
-			game_over = True
-			maze[y][x] = w
-		sense.set_pixels(sum(maze,[]))
-		sleep(0.01)
-		maze[y][x] = b
+
+def game_start():
+    while not game_over:
+        pitch = sense.get_orientation()['pitch']
+        roll = sense.get_orientation()['roll']
+        x, y = move_marble(pitch, roll, x, y)
+        if maze[y][x] == g:
+            sense.show_message("Level 2")
+            maze = maze2
+        if maze[y][x] == g:
+            sense.show_message("Level 3")
+            maze2 = maze3
+        if maze[y][x] == g:
+            sense.show_message("Win!!")
+            game_over = True
+            maze[y][x] = w
+        sense.set_pixels(sum(maze, []))
+        sleep(0.01)
+        maze[y][x] = b
