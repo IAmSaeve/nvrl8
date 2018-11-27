a8e335e3025f7520c91ab11b0a422b7138e8c2c2
Fearce <thekev93@gmail.com>
2018-11-27 12:06:32 +0100

    Update MazeGame.py

--- Python/MazeGame.py
+++ Python/MazeGame.py
@@ -72,16 +72,16 @@ def move_marble(pitch, roll, x, y):
     new_y = y
     if 1 < pitch < 179 and x != 0:
         new_x -= 1
-        print("moving -x" + pitch)
+        print("moving -x" + str(pitch))
     if 181 < pitch < 359 and x != 7:
         new_x += 1
-        print("moving +x" + pitch)
+        print("moving +x" + str(pitch))
     if 1 < roll < 179 and y != 7:
         new_y += 1
-        print("moving +y" + roll)
+        print("moving +y" + str(roll))
     if 181 < roll < 359 and y != 0:
         new_y -= 1
-        print("moving -y" + roll)
+        print("moving -y" + str(roll))
     new_x, new_y = check_wall(x, y, new_x, new_y)
     return new_x, new_y
 
