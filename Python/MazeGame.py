b93c0d3b995824146e373f7bb50b322a6ab47019
Fearce <thekev93@gmail.com>
2018-11-27 11:14:16 +0100

    currentMaze

--- Python/MazeGame.py
+++ Python/MazeGame.py
@@ -9,6 +9,7 @@ b = (0, 0, 0)
 w = (255, 255, 255)
 g = (218, 165, 32)
 ng = (218, 165, 32)
+currentMaze = "maze"
 
 x_pos = 1
 y_pos = 1
@@ -76,23 +77,25 @@ def move_marble(pitch, roll, x, y):
 
 
 def game_start():
-    global game_over, x_pos, y_pos, maze, maze2, maze3
+    global game_over, x_pos, y_pos, maze, maze2, maze3, currentMaze
     while not game_over:
         pitch = sense.get_orientation()['pitch']
         roll = sense.get_orientation()['roll']
         x_pos, y_pos = move_marble(pitch, roll, x_pos, y_pos)
-        if maze[y_pos][x_pos] == g and maze == maze:
+        if maze[y_pos][x_pos] == g and currentMaze == "maze":
             #subprocess.Popen(["ps aux | grep omxplayer | grep -v grep | awk '{print $2}' | xargs kill"], shell=True)
             sense.show_message("Level 2")
             maze = maze2
+            currentMaze = "maze2"
             x_pos = m2x
             y_pos = m2y
-        if maze[y_pos][x_pos] == g and maze == maze2:
+        if maze[y_pos][x_pos] == g and currentMaze == "maze2":
             sense.show_message("Level 3")
             maze2 = maze3
+            currentMaze = "maze3"
             x_pos = m3x
             y_pos = m3y
-        if maze[y_pos][x_pos] == g and maze == maze3:
+        if maze[y_pos][x_pos] == g and currentMaze == "maze3":
             sense.show_message("Win!!")
             game_over = True
         maze[y_pos][x_pos] = w
