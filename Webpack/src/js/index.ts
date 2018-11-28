633fedcb8ddeaea526fd15dedc349d036f2e1da9
michaelSteinmejer <33892912+michaelSteinmejer@users.noreply.github.com>
2018-11-28 22:03:40 +0100

    Revert "blah"
    This reverts commit b2fe44da953254502b0b956b9df5db97c95d3f91.

--- Webpack/src/js/index.ts
+++ Webpack/src/js/index.ts
@@ -1,6 +1,4 @@
 
-import * as cors from "cors";
-import * as express from "express";
 import axios, {AxiosError, AxiosResponse } from "../../node_modules/axios/index";
 import { Leg } from "../js/leg";
 import { Trip } from "../js/trip";
@@ -10,38 +8,10 @@ const node = document.createElement("li") as HTMLLIElement;
 const span = document.createElement("span") as HTMLSpanElement;
 const GetAButton: HTMLButtonElement = document.getElementById("GetAButton") as HTMLButtonElement;
 
-// var router = express.Router();
-const options: cors.CorsOptions = {
-  allowedHeaders: "*",
-  credentials: true,
-  methods: "*",
-  origin: "*",
-  preflightContinue: false,
-
-};
-const router = express.Router();
-router.use(cors(options));
-
-const url  = "http://xmlopen.rejseplanen.dk/bin/rest.exe/trip?originId=8600617" +
-            "&destCoordX=12565562&destCoordY=55673063&destCoordName=K%C3%B8benhavn%20H&date=28.11.18" +
-            "&time=10:58&useBus=0&format=json";
-const xhr  = new XMLHttpRequest();
-xhr.open("GET", url, true);
-xhr.setRequestHeader("X-GET", "GET");
-xhr.onload = () => {
-    const TripArray: Trip[] = JSON.parse(xhr.responseText);
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        console.table(TripArray);
-    } else {
-        console.error(TripArray);
-    }
-};
-xhr.send(null);
-
 // Import stop locations.
-// import * as data from "../Data/stops.json";
-// const stopArray: IStop[] = data.default as IStop[];
-// console.log(stopArray);
+import * as data from "../Data/stops.json";
+const stopArray: IStop[] = data.default as IStop[];
+console.log(stopArray);
 
 function onSignIn(googleUser: any) {
     const profile = googleUser.getBasicProfile();
