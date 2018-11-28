7633bf74acbc454c953c33dd88c5fe3b29eb1371
michaelSteinmejer <33892912+michaelSteinmejer@users.noreply.github.com>
2018-11-28 12:48:41 +0100

    Trip rettet
    rettet

--- Webpack/src/js/trip.ts
+++ Webpack/src/js/trip.ts
@@ -1,12 +1,17 @@
 import { Leg } from "../js/leg";
 
 export class Trip {
+    public Alternative: boolean;
+    public Cancelled: boolean;
+    public Valid: boolean;
+    public Legs: Leg[];
 
-public Legs: Leg[];
-
- constructor(legs: Leg[]) {
+ constructor(legs: Leg[], alternative?: boolean, valid?: boolean, cancelled?: boolean) {
     this.Legs = legs;
 
+    this.Alternative = alternative;
+    this.Valid = valid;
+    this.Cancelled = cancelled;
 }
 
 }
