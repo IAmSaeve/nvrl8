8ebc22b734dc595c22c49211487ad0a93d836d00
Sebastian Petersen <sebastian@srmail.dk>
2018-11-29 19:27:52 +0100

    Moved IStop to seperate file

---
+++ Webpack/src/js/Interface/IStop.ts
@@ -0,0 +1,10 @@
+export interface IStop {
+    stop_id: string;
+    stop_code: string;
+    stop_name: string;
+    stop_desc: string;
+    stop_lat: string;
+    stop_lon: string;
+    location_type: string;
+    parent_station: string;
+}
