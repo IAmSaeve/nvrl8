3cde7663e71c9c129f632d40c90e00122ae244f2
michaelSteinmejer <33892912+michaelSteinmejer@users.noreply.github.com>
2018-11-29 12:17:01 +0100

    webService(maybe)

---
+++ RejseplanAPI/RejseplanAPI/Model/leg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RejseplanAPI.Model
+{
+    public class Leg
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public Origin Origin { get; set; }
+        public Destination Destination { get; set; }
+
+        public Leg(string name, string type, Origin origin, Destination destination)
+        {
+            Name = name;
+            Type = type;
+            Origin = origin;
+            Destination = destination;
+        }
+    }
+}
