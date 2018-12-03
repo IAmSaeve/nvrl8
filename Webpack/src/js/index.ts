9beff7e4b02946659a1061ca99ca7754123e21e2
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2018-12-03 09:26:19 +0100

    removed html generator for login

--- Webpack/src/js/index.ts
+++ Webpack/src/js/index.ts
@@ -82,19 +82,6 @@ function GetTripsAxios(): void {
 }
 
 
-function GetLoginPage(): void {
-    // OPRETTER HTML TIL LOGIN
-    let html = "";
-    html = "<header class='container fluid col-lg-12'>";
-    html += "<div class='col-lg-6'>";
-    html += "<h1 class='color'>Nvr<span class='forsent'>L8</span></h1>";
-    html += "</div></header>";
-    html += "<form class='col-lg-4 offset-lg-5 formcontainer'>";
-    html += "<div class='g-signin2' data-onsuccess='onSignIn' id='logInDiv'></div>";
-
-    content.innerHTML = html;
-}
-
 function GetProfilePage(): void {
     // OPRETTER HTML TIL PROFILE
     let html = "";
