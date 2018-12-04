8449a388bf3e53aa5a07ff5906344f2cdb764fc0
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2018-12-04 11:34:47 +0100

    login

--- nvrl8-ws/Controllers/SettingController.cs
+++ nvrl8-ws/Controllers/SettingController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using nvrl8_ws.Model;
 
 namespace nvrl8_ws.Controllers
 {
@@ -11,11 +13,32 @@ namespace nvrl8_ws.Controllers
     [ApiController]
     public class SettingController : ControllerBase
     {
+        private static string conn =
+            "Server=tcp:nvrl8.database.windows.net,1433;InitialCatalog=nvrl8;Persist SecurityInfo=False;UserID=nvrl8admin;Password=p@$$W0RD;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;ConnectionTimeout=30;";
         // GET: api/Setting
         [HttpGet]
-        public IEnumerable<string> Get()
+        public Settings GetACustomer(int id)
         {
-            return new string[] { "value1", "value2" };
+            using (SqlConnection dbConnection = new SqlConnection(conn))
+            {
+                dbConnection.Open();
+
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Settings INNER JOIN Users ON Settings.ID=Users.LogID WHERE ID = @id", dbConnection))
+                {
+                    command.Parameters.Add(new SqlParameter("id", id));
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            return new Settings(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
+                        }
+                    }
+                }
+
+                dbConnection.Close();
+
+                return null;
+            }
         }
 
         // GET: api/Setting/5
