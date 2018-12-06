c326830888f0d443e0f5a0524d9da471ed9bfce0
Fearce <thekev93@gmail.com>
2018-12-06 10:12:57 +0100

    updayte settings model class

--- nvrl8-ws/Model/Settings.cs
+++ nvrl8-ws/Model/Settings.cs
@@ -11,18 +11,26 @@ namespace nvrl8_ws.Model
         public string Origin { get; set; }
         public string Destination { get; set; }
         public string GoTime { get; set; }
+        public string AwakeTime { get; set; }
+        public string OriginX { get; set; }
+        public string OriginY { get; set; }
+        public string UseBus { get; set; }
 
-        public Settings(int id, string origin, string destination, string goTime)
+        public Settings(int id, string origin, string destination, string goTime, string awakeTime, string originX, string originY, string useBus)
         {
             Id = id;
             Origin = origin;
             Destination = destination;
             GoTime = goTime;
+            AwakeTime = awakeTime;
+            OriginX = originX;
+            OriginY = originY;
+            UseBus = useBus;
         }
 
         public Settings()
         {
-            
+
         }
     }
 }
