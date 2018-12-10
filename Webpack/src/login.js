7185df71749344996749bfd5ad9b64ab901ece8b
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2018-12-10 13:15:31 +0100

    edit login

--- Webpack/src/login.js
+++ Webpack/src/login.js
@@ -1,20 +1,25 @@
 function onSignIn(googleUser) {
+    var uri = "https://localhost:5001/api/user";
+    var data = { name: profile.getName, email: profile.getEmail, imageURL: profile.getImageUrl};
+
+    axios.post(uri, data).then(() => {
+
+    })
+    .catch((error) => { console.log(error); })
+    console.log(name)
+    console.log(email)
     var profile = googleUser.getBasicProfile();
-    
+
     console.log('Name: ' + profile.getName());
 
     console.log('Image URL: ' + profile.getImageUrl());
 
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+
     
-   location.href="profile.htm";
+
+    //location.href = "profile.htm";
 }
 
-var uri = "https://nvrl8.azurewebsites.net/api/user";
-var data = {name:profile.getName, email:profile.getEmail, imageURL:profile.getImageUrl};
-
-axios.post(uri,data).then(()=> {
-
-})
-.catch((error) => {console.log(error);})
 
