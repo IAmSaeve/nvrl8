903ec01d574ee37ba12e41a2134191d884f9c45a
Fearce <thekev93@gmail.com>
2018-12-11 09:25:33 +0100

    destination name in index.ts on settings page

--- Webpack/src/js/index.ts
+++ Webpack/src/js/index.ts
@@ -223,17 +223,23 @@ function GetSettingsAxios(): void {
             const nodeBus = document.createElement("li");
             const nodeGo = document.createElement("li");
             const nodeAwake = document.createElement("li");
-
             
             nodeOrigin.appendChild(document.createTextNode(`Fra: ${settings.origin}`));
             nodeOrigin.className= "topmargin";
-            nodeDest.appendChild(document.createTextNode(`Til: ${settings.destination}`));
-            nodeDest.className= "topmargin";
+            let destName = "";
+            console.log(settings.destination);
+            stopArray.forEach((e) => {
+                if (+e.stop_id === +settings.destination) {
+                    destName = e.stop_name;
+                }
+            });
+            nodeDest.appendChild(document.createTextNode(`Til: ${destName}`));
+            nodeDest.className = "topmargin";
             nodeX.appendChild(document.createTextNode(`Koordinat x: ${settings.originX}`));
-            nodeX.className= "topmargin";
+            nodeX.className = "topmargin";
             nodeY.appendChild(document.createTextNode(`Koordinat y: ${settings.originY}`));
-            nodeY.className= "topmargin";
-            if(settings.useBus===1)
+            nodeY.className = "topmargin";
+            if(settings.useBus === 1)
             {
                 nodeBus.appendChild(document.createTextNode(`Bus: Ja`));
                 nodeBus.className= "topmargin";
