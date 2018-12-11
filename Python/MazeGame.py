4bcd9da1a8df962197636e472e236efb3fed0de9
Fearce <thekev93@gmail.com>
2018-12-11 10:42:19 +0100

    maze11 fix

--- Python/MazeGame.py
+++ Python/MazeGame.py
@@ -119,7 +119,7 @@ maze11 = [[r, b, r, b, r, b, r, r],
          [r, b, r, r, r, r, b, b],
          [b, b, r, b, r, b, b, r],
          [r, b, b, b, b, b, b, r],
-         [r, r, rb r, b, r, r, r]]
+         [r, r, b, r, b, r, r, r]]
 
 maze12 = [[b, b, r, r, r, r, r, r],
          [r, b, r, r, r, b, b, r],
